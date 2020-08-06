a2015efee0fc95391514a0d6bd33e4a4980b3142
David Jones <kitibyte@protonmail.com>
2020-08-07 00:20:58 +0100

    Filter stations client side

--- hooks/use-station-suggestions.ts
+++ hooks/use-station-suggestions.ts
@@ -1,23 +1,26 @@
-import { encodeName } from 'models/station';
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import fuzzysort from 'fuzzysort';
+import { Dispatch, SetStateAction, useMemo, useState } from 'react';
 import useSWR from 'swr';
 
 /**
- * Returns a list of station names matching the search term.
+ * Returns a list of station names matching the search term, and a function to update the search term.
  * Stale data is returned until the current request has completed.
  */
-export default function useStationSuggestions(): [suggestions: string[], setSearchTerm: Dispatch<SetStateAction<string>>] {
+export default function useStationSuggestions(): [
+  string[],
+  Dispatch<SetStateAction<string>>
+] {
+  const { data: stations = [] } = useSWR(`/api/stations`);
   const [searchTerm, setSearchTerm] = useState('');
-  const [suggestions, setNames] = useState<string[]>([]);
-  const { data } = useSWR<string[]>(
-    `/api/stations?q=${encodeName(searchTerm)}`
-  );
 
-  useEffect(() => {
-    if (data) {
-      setNames(data);
-    }
-  }, [data]);
+  const targets = useMemo(() => {
+    return stations.map(fuzzysort.prepare);
+  }, [stations]);
+
+  const suggestions = useMemo(() => {
+    const results = fuzzysort.go(searchTerm, targets, { limit: 10 });
+    return results.map((result) => result.target);
+  }, [searchTerm, targets]);
 
   return [suggestions, setSearchTerm];
 }
--- pages/api/stations/index.ts
+++ pages/api/stations/index.ts
@@ -4,12 +4,15 @@ import castArray from 'lodash/castArray';
 import { decodeName } from 'models/station';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const targets = Object.keys(stations).map(fuzzysort.prepare);
+const names = Object.keys(stations);
+const targets = names.map(fuzzysort.prepare);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const search = decodeName(castArray(req.query.q)[0]);
-  const results = fuzzysort.go(search, targets, {
-    limit: 10,
-  });
-  res.json(results.map((result) => result.target));
+  if (req.query.q === undefined) {
+    res.json(names);
+  } else {
+    const search = decodeName(castArray(req.query.q)[0]);
+    const results = fuzzysort.go(search, targets, { limit: 10 });
+    res.json(results.map((result) => result.target));
+  }
 };
