31f852c0ee2d44d7b75557f2fc2e9ec24853bf55
David Jones <kitibyte@protonmail.com>
2020-08-22 00:09:53 +0100

    Use search box on Station page (#20)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,29 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+
   purge: ['{components,pages}/**/*.{js,tsx}'],
+
   theme: {
     fontFamily: {
       casual: ['Kalam', 'cursive'],
       marker: ['Permanent Marker', 'cursive'],
       sans: ['Inter', 'sans-serif'],
     },
+
     extend: {
       colors: {
         'nre-corporate-blue': 'rgb(0, 51, 100)',
       },
     },
   },
+
   variants: {
+    borderColor: ['responsive', 'hover', 'focus', 'focus-within'],
     boxShadow: ['responsive', 'hover', 'focus', 'focus-within'],
     display: ['responsive', 'group-hover', 'group-focus'],
   },
+
   plugins: [require('@tailwindcss/typography')],
 };
