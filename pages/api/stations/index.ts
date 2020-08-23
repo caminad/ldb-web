9d0056b13a33c9f8c64efc60120a950273524e28
David Jones <kitibyte@protonmail.com>
2020-08-23 14:42:07 +0100

    Remove unused filtering from /api/stations (#27)
    Filtering is done client-side in useStationSuggestions.

--- pages/api/stations/index.ts
+++ pages/api/stations/index.ts
@@ -1,18 +1,8 @@
 import stations from 'data/stations.json';
-import fuzzysort from 'fuzzysort';
-import castArray from 'lodash/castArray';
-import { decodeName } from 'models/station';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const names = Object.keys(stations);
-const targets = names.map(fuzzysort.prepare);
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.query.q === undefined) {
-    res.json(names);
-  } else {
-    const search = decodeName(castArray(req.query.q)[0]);
-    const results = fuzzysort.go(search, targets, { limit: 10 });
-    res.json(results.map((result) => result.target));
-  }
+export default async (_: NextApiRequest, res: NextApiResponse) => {
+  res.json(names);
 };
