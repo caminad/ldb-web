3791dd61b7144ddf01557c383317f82c3069a960
David Jones <kitibyte@protonmail.com>
2020-09-05 18:28:38 +0100

    Switch to external GraphQL data source
    https://github.com/kitibyte/ldb-graphql

--- hooks/useStationSuggestions.ts
+++ hooks/useStationSuggestions.ts
@@ -1,16 +1,19 @@
 import fuzzysort from 'fuzzysort';
-import { useMemo } from 'react';
-import useSWR from 'swr';
+import { useEffect, useMemo, useState } from 'react';
 
 /**
  * Returns a list of station names matching the search term, and a function to update the search term.
  */
 export default function useStationSuggestions(searchTerm: string): string[] {
-  const { data: stations = [] } = useSWR(`/api/stations`);
+  const [targets, setTargets] = useState<(Fuzzysort.Prepared | undefined)[]>(
+    []
+  );
 
-  const targets = useMemo(() => {
-    return stations.map(fuzzysort.prepare);
-  }, [stations]);
+  useEffect(() => {
+    import('data/stations.json').then(({ default: stations }) => {
+      setTargets(Object.keys(stations).map(fuzzysort.prepare));
+    });
+  }, []);
 
   const suggestions = useMemo(() => {
     const results = fuzzysort.go(searchTerm, targets, {
