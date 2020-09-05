c81babfb04f4bb5d71097d8b7ee54b4fcf26a939
David Jones <kitibyte@protonmail.com>
2020-09-05 14:24:30 +0100

    Upgrade Tailwind

--- components/ServiceTime.tsx
+++ components/ServiceTime.tsx
@@ -6,7 +6,7 @@ export default function ServiceTime(props: {
 }) {
   const offSchedule = props.estimate !== 'On time';
   return (
-    <span className="font-features tabular-numbers stylistic-alternates font-medium">
+    <span className="tabular-nums font-medium">
       <span className={clsx({ 'line-through text-gray-500': offSchedule })}>
         {props.children}
       </span>{' '}
