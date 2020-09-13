d066435420bbba7074f628068cf7a7c8d8712c48
David Jones <kitibyte@protonmail.com>
2020-09-13 15:11:47 +0100

    Use genql to generate GraphQL client

--- models/Station.ts
+++ models/Station.ts
@@ -1,15 +1,3 @@
-import Service from 'models/Service';
-
-export default interface Station {
-  crs: string;
-  locationName: string;
-  generatedAt: string;
-  nrccMessages?: string[];
-  platformAvailable: boolean;
-  trainServices?: Service[];
-  busServices?: Service[];
-}
-
 export function decodeName(x: string | string[] | undefined): string {
   return decodeURIComponent(first(x) ?? '').replace(/_/g, ' ');
 }
