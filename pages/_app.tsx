2b7d36baaf84f5eba5c6947d021d4e3076e02c74
David Jones <kitibyte@protonmail.com>
2020-10-15 14:02:55 +0100

    Fix build-time warning (#146)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import ErrorBoundary from 'components/ErrorBoundary';
 import { AppProps } from 'next/app';
+import { SSRProvider } from 'react-aria';
 import 'styles/inter.css';
 import 'styles/tailwind.css';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ErrorBoundary>
-      <Component {...pageProps} />
-    </ErrorBoundary>
+    <SSRProvider>
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
+    </SSRProvider>
   );
 }
